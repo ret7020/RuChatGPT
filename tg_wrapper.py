c70b4f02f230fe1b2c75434130ac524d8a575b5a
ret7020 <ret7020@gmail.com>
2022-12-28 18:50:07 +0300

    MVP

--- tg_wrapper.py
+++ tg_wrapper.py
@@ -22,7 +22,7 @@ async def send_welcome(message: types.Message):
     reply = "No sessions"
     if user_id in SESSIONS:
         if len(SESSIONS[user_id]) > 0:
-            reply = f"Session id: {SESSIONS[user_id].session_id}"
+            reply = f"Session id: {SESSIONS[user_id][0].session_id}"
     await message.answer(reply)
 
 @dp.message_handler(commands=["clears"])
@@ -46,6 +46,25 @@ async def send_welcome(message: types.Message):
         await message.answer("🛠Starting job on creating new session...")
         SESSIONS[user_id] = [UserSession()]
         await message.answer("✅ Session started; You can chat now")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    answered = False
+    user_id = message.from_user.id
+    if user_id in SESSIONS:
+        if len(SESSIONS[user_id]) > 0:
+            res = neural_predictor.answer("H", SESSIONS[user_id][0].chat_history_ids, message.text) # send message to bot
+            SESSIONS[user_id][0].chat_history_ids = res[2] # save history to session
+            answer = neural_predictor.answer("G", SESSIONS[user_id][0].chat_history_ids)
+            SESSIONS[user_id][0].chat_history_ids = answer[2] # save history
+            answered = True
+            if answer[1]:
+                await message.answer(answer[1])
+            else:
+                await message.answer("<-- БОТ РЕШИЛ ОТВЕТИТЬ ПУСТОТОЙ -->")
+    if not answered:
+        await message.answer("Create session first")
+
 
 
 
